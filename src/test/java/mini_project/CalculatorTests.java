7d6571ce0645301cf51ce90a11a495a706d4db26
Prasanth Kumar <prasanthgajjala007@gmail.com>
2021-03-14 18:50:25 +0530

    Added JenkinsFile

---
+++ src/test/java/mini_project/CalculatorTests.java
@@ -0,0 +1,45 @@
+import org.junit.*;
+
+public class CalculatorTests {
+
+    private calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new calculator();
+    }
+
+    @Test
+    public void testSqRt() {
+        double a = 4;
+        double expectedResult = 2.0;
+        long result = calculator.sqRt(a);
+        Assert.assertEquals(expectedResult, result);
+        ;
+    }
+
+    @Test
+    public void testFcat() {
+        double a = 5;
+        double expectedResult = 120.0;
+        double result = calculator.fact(a);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testLog() {
+        double a = 10;
+        double expectedResult = 1.0;
+        double result = calculator.log(a);
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testPower() {
+        double a = 4;
+        double b = 6;
+        double expectedResult = 4096.0;
+        double result = calculator.fact(a, b);
+        Assert.assertEquals(expectedResult, result, 0.00005);
+    }
+}
